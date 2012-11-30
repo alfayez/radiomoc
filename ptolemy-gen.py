dd1fd1827f1be9a5f4863c50fea899dc86de7f79
Almohanad Fayez <you@example>
2012-11-29 20:34:16 -0500

    Fix some more location stuff and add director capability

--- ptolemy-gen.py
+++ ptolemy-gen.py
@@ -14,15 +14,23 @@ XML_HEADER2 = """<!DOCTYPE entity PUBLIC \"-//UC Berkeley//DTD MoML 1//EN\"
 \"http://ptolemy.eecs.berkley.edu/xml/dtd/MoML_1.dtd\">
 """
 PARAM_COLOR = [0.0, 0.0, 1.0, 1.0]
-PARAM_ORIG  = [1,1]
-PARAM_STEP  = 5
+PARAM_ORIG  = [1,0]
+PARAM_STEP  = 15
 
-BLOCK_ORIG  = [10, 1]
-BLOCK_STEP  = 15 
+BLOCK_ORIG  = [0, 1]
+BLOCK_STEP  = 50 
+
+DIRECT_LOC = [200, 10]
+ERROR_LOC = "{-1, -1}"
 
 X_AXIS = 0
 Y_AXIS = 1
 
+PARAM  = 0
+BLOCK  = 1
+DIRECT = 2
+
+SDF = 0
 #XML_INIT = "entity "
 class ptolemy_writer:
     def __init__(self, outfile_name, model_name):
@@ -52,7 +60,6 @@ class ptolemy_writer:
         if (value_str != "None"):
             node.setAttribute("value", value_str)        
 
-        #self.top_element.appendChild(maincard)
         return node
 
     def write_to_xmlfile(self):
@@ -61,10 +68,6 @@ class ptolemy_writer:
         ser.domConfig.setParameter('format-pretty-print', True)
         ser.writeToURI(self.doc, 'tmp_xml_read.xml')
         
-        #outfile_temp = file('tmp_xml_read.xml', 'w')
-        #outfile_temp.write(self.doc.toprettyxml())
-        #outfile_temp.close()
-
         infile_temp = file ('tmp_xml_read.xml', 'r')
 
         line = infile_temp.readline()
@@ -102,18 +105,45 @@ class ptolemy_writer:
         infile_temp.close()
         print "\n"
 
-    def write_parameter(self, name, value):
-        node1 = pgen.write_element(PROP, "period_time", CLASS_PARAMETER, "400")
-        node2 = pgen.write_element(PROP, NAME_ICON, CLASS_ICON, NONE)
-        node3 = pgen.write_element(PROP, NAME_COLOR, CLASS_COLOR, VAL_COLOR_PARAMETER)
+    def ptolemy_location_update(self, req_type):
 
-        self.param_loc[Y_AXIS] = self.param_loc[Y_AXIS] + PARAM_STEP
-        loc_str = "{" + str(self.param_loc[X_AXIS]) + ", " + str(self.param_loc[Y_AXIS]) + "}"
-        node4 = pgen.write_element(PROP, NAME_LOCATION, CLASS_LOCATION, loc_str)
+        if req_type is PARAM:
+            self.param_loc[Y_AXIS] = self.param_loc[Y_AXIS] + PARAM_STEP
+            loc_str = "{" + str(self.param_loc[X_AXIS]) + ", " + str(self.param_loc[Y_AXIS]) + "}"
+        elif req_type is BLOCK:
+            self.param_loc[X_AXIS] = self.param_loc[X_AXIS] + PARAM_STEP
+            loc_str = "{" + str(self.param_loc[X_AXIS]) + ", " + str(self.param_loc[Y_AXIS]) + "}"
+        elif req_type is DIRECT:
+            loc_str = "{" + str(DIRECT_LOC[X_AXIS]) + ", " + str(DIRECT_LOC[Y_AXIS]) + "}"            
+        else:
+            exit("ERROR: Found in ptolemy_location_update method. Parameter passed is incompatible\n")
+        return loc_str
+        
+    def write_to_ptolemy_file(self, type_str, name, value):
 
-        node1.appendChild(node2)
-        node2.appendChild(node3)
-        node1.appendChild(node4)
+        if type_str is PARAM:
+
+            node1 = self.write_element(PROP, name, CLASS_PARAMETER, value)
+            node1 = self.write_element(PROP, name, CLASS_PARAMETER, "None")
+            node2 = self.write_element(PROP, NAME_ICON, CLASS_ICON, "None")
+            node3 = self.write_element(PROP, NAME_COLOR, CLASS_COLOR, VAL_COLOR_PARAMETER)
+
+            loc_str = self.ptolemy_location_update(PARAM)
+            node4 = self.write_element(PROP, NAME_LOCATION, CLASS_LOCATION, loc_str)
+
+            node1.appendChild(node2)
+            node2.appendChild(node3)
+            node1.appendChild(node4)
+            
+        elif type_str is BLOCK:
+            print "NLOCK"
+        elif type_str is DIRECT:
+            node1 = self.write_element(PROP, NAME_SDF, CLASS_SDF, "None")
+            loc_str = self.ptolemy_location_update(DIRECT)
+            node2 = self.write_element(PROP, NAME_LOCATION, CLASS_LOCATION, loc_str)
+            node1.appendChild(node2)
+        else:
+            exit("ERROR: Found in write_to_ptolemy method. Parameter passed is incompatible\n")
 
         return node1
 if __name__ == "__main__":
@@ -123,9 +153,21 @@ if __name__ == "__main__":
     pgen = ptolemy_writer(filename, model_name)
     pgen.write_element("property", "_createBy", "ptolemy.kernel.attributes.VersionAttribute", "8.0.1_20101021")
 
-    node = pgen.write_parameter("period_time", "400")
+    node1 = pgen.write_to_ptolemy_file(PARAM, "period_time", "4000")
+    node2 = pgen.write_to_ptolemy_file(PARAM, "carrier_freq", "4.0")
+    node3 = pgen.write_to_ptolemy_file(PARAM, "carrier_phase", "0")
+    node4 = pgen.write_to_ptolemy_file(PARAM, "sampling_freq", "400")
+    node5 = pgen.write_to_ptolemy_file(PARAM, "symbol_time", "2")
 
-    pgen.top_element.appendChild(node)
+    pgen.top_element.appendChild(node1)
+    pgen.top_element.appendChild(node2)
+    pgen.top_element.appendChild(node3)
+    pgen.top_element.appendChild(node4)
+    pgen.top_element.appendChild(node5)
+
+    node1 = pgen.write_to_ptolemy_file(DIRECT, NAME_SDF, "None")
+
+    pgen.top_element.appendChild(node1)
 
     pgen.write_to_xmlfile()
     pgen.print_xmlfile()
--- ptolemy_param.py
+++ ptolemy_param.py
@@ -4,12 +4,13 @@
 NAME_ICON     = "_icon"
 NAME_COLOR    = "_color"
 NAME_LOCATION = "_location"
-
+NAME_SDF      = "SDF Director"
 # Ptolemy Classes
 CLASS_PARAMETER = "ptolemy.data.expr.Parameter"
 CLASS_ICON      = "ptolemy.vergil.icon.ValueIcon"
 CLASS_COLOR     = "ptolemy.actor.gui.ColorAttribute"
 CLASS_LOCATION  = "ptolemy.kernel.util.Location"
+CLASS_SDF       = "ptolemy.domains.sdf.kernel.SDFDirector"
 
 # Ptolemy Parameters
 VAL_COLOR_PARAMETER = "{0.0, 0.0, 1.0, 1.0}"
