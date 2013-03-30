a9b25985e0737d6f6ad91d851d300f7ba8bb75d1
Almohanad Fayez <you@example>
2013-03-30 17:19:41 -0400

    coordinate with pc

---
+++ ce_interface.py
@@ -0,0 +1,25 @@
+import sys, string, types, os, copy, time
+import getopt
+import numpy as np
+import scipy
+import fractions
+import subprocess
+
+from ptolemy_gen  import *
+from gnuradio_gen import *
+from lp_gen       import *
+from occam_parser import *
+import occam_parser as occam
+
+class ce_interface:
+    def __init__(self):
+        self.out_file_vect = []
+        self.token_vect    = []
+        self.alloc_vect    = []
+        self.vect_vect     = []
+        self.run_vect      = []
+    def set_ranges(self, vect_range):
+        self.vect_range
+    
+if __name__ == "__main__":
+    
