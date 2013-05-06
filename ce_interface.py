2415398c9aac674cdf286c904384f1032ff661f6
Almohanad Fayez <you@example>
2013-05-06 03:51:04 -0400

    saved CSP Sim Exp graphs

--- ce_interface.py
+++ ce_interface.py
@@ -249,7 +249,7 @@ class ce_interface:
 if __name__ == "__main__":
     print "Before system call"
     num_for_average     = 10
-    start_vect          = 12
+    start_vect          = 0
     vectorization_times = 15
     run_time_duration   = 60*5
     #run_time_duration   = 10 
@@ -285,8 +285,9 @@ if __name__ == "__main__":
             #result = os.system(command_str)
             #print "command= ", command_str2
 
-            proc = subprocess.Popen(command_str2, shell=False)
-            result = proc.wait()
+            # runs framework
+            #proc = subprocess.Popen(command_str2, shell=False)
+            #result = proc.wait()
 
 
             #out, err = proc.communicate()
